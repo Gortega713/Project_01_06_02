2ae741c40f100edb317d6071bb842e3dfa4a6c71
Unknown <Gortega713@west-mec.org>
2018-08-17 17:23:39 -0700

    Check Placeholders
    Added a function and event listeners to check for placeholders in the street address field

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 /*
-      Project 01_06_01
+      Project 01_06_02
 
       Author: Gabriel Ortega
-      Date: 8.15.16  
+      Date: 8.16.16  
 
       Filename: script.js
 */
 
 "use strict"
 
-var formValidity = true;
+var formValidity = false;
 
 // Function to validate form on submit
 
@@ -23,9 +23,13 @@ function validateForm(evt) {
     }
 
     validateRequired();
+
+    if (formValidity === true) {
+        document.getElementsByTagName("form")[0].submit();
+    }
 }
 
-// Function to  Validate required fields
+// Function to  validate required fields (all)
 
 function validateRequired() {
     var inputElements = document.querySelectorAll("#contactinfo input");
@@ -39,22 +43,23 @@ function validateRequired() {
         // Loop through all of the input elements and set "[i]" to the current element (which is currently null)
         for (var i = 0; i < elementLength; i++) {
             currentElement = inputElements[i];
-            
+
             // Test for blank fields
             if (currentElement.value === "") {
                 currentElement.style.background = invColor;
                 fieldsetValidity = false;
-               
+
             } else {
                 currentElement.style.background = "white";
             }
         }
-        
+
         // Throw the message if there are fieldsets which are invalid/empty (fieldsetValidity is set to "false")
         if (fieldsetValidity === false) {
-            throw "Please fill in all fields"
+            throw "Please fill in all fields";
         } else {
             fieldsetValidity = true;
+            formValidity = true;
         }
     } catch (err) {
         errorDiv.innerHTML = err;
@@ -62,6 +67,51 @@ function validateRequired() {
         errorDiv.style.color = "red";
     }
 }
+
+// Remove fallback placeholder test
+
+function zeroPlaceholder() {
+    var addressBox = document.getElementById("addrinput");
+    addressBox.style.color = "black";
+    if (addressBox.value === addressBox.placeholder) {
+        addressBox.value = "";
+    }
+}
+
+// Restore placeholder text if box contains no user entry
+
+function checkPlaceholder() {
+    var addressBox = document.getElementById("addrinput");
+    if (addressBox.value === "") {
+        addressBox.style.color = "rgb(178, 184, 183)"
+        addressBox.value = addressBox.placeholder;
+    }
+}
+
+// Restore placeholder text if box contains no user entry
+
+function generatePlaceholder() {
+    if (!Modernizr.input.placeholder) {
+        var addressBox = document.getElementById("addrinput");
+        addressBox.style.color = "rgb(178, 184, 183)"
+        addressBox.value = addressBox.placeholder;
+        if (addressBox.addEventListener) {
+            addressBox.addEventListener("focus", zeroPlaceholder, false);
+            addressBox.addEventListener("blur", checkPlaceholder, false);
+        } else if (addressBox.attachEvent) {
+            addressBox.attachEvent("onfocus", zeroPlaceholder);
+            addressBox.attachEvent("blur", checkPlaceholder);
+        }
+    }
+}
+
+// Function to set up page
+
+function setUpPage() {
+    createEventListeners();
+    generatePlaceholder();
+}
+
 
 // Function to create all event listeners
 
@@ -76,5 +126,8 @@ function createEventListeners() {
     }
 }
 
-// Event listener for when the page loads
-window.addEventListener("load", createEventListeners);
+if (window.addEventListener) {
+    window.addEventListener("load", setUpPage, false);
+} else if (window.attachEvent) {
+    window.attachEvent("onload", setUpPage);
+}
